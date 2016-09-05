345a6406ec85c1cc2d0c9fce889c4f72d2c7692c
Juri Pebriant <junkuwieazy@yahoo.com>
2016-09-05 07:17:14 +0700

    Made Simple Connection Php
    I just share what i know...!

--- Connection.php
+++ Connection.php
@@ -1,76 +1,23 @@
 <?php
-/**
- * @author Evin Weissenberg
- */
+error_reporting(0);
+class KoneksiModel {
 
-class Connection {
+  protected $koneksi;
+  public $nama_database = 'databaseName';
+  public $driver_server = 'mysql';
+  public $server_database = 'localhost';
+  public $port_server = '3306';
+  public $username_koneksi = 'username';
+  public $password_koneksi = 'password';
 
-    public static $db = 'production'; //production, staging, development.
+  public function KoneksiDatabase() {
 
-    //PRODUCTION
-    public static $db_host_production = '';
-    public static $db_username_production = '';
-    public static $db_password_production = '';
-    public static $db_name_production = '';
-    public static $db_production_port = '3306';
-
-    //STAGING
-    public static $db_host_staging = '';
-    public static $db_username_staging = '';
-    public static $db_password_staging = '';
-    public static $db_name_staging = '';
-    public static $db_staging_port = '3306';
-
-    //DEVELOPMENT
-    public static $db_host_development = '';
-    public static $db_username_development = '';
-    public static $db_password_development = '';
-    public static $db_name_development = '';
-    public static $db_development_port = '3306';
-
-
-    public static function __construct($db) {
-
-        if (self::$db == 'production') {
-
-            $db = new mysqli(self::$db_host_production, self::$db_username_production, self::$db_password_production, self::$db_name_production);
-
-            if ($db->connect_errno) {
-                printf("Connection failed: %s\n", $db->connect_error);
-                return False;
-            }
-
-            return $db;
-
-        } elseif (self::$db == 'staging') {
-
-            $db = new mysqli(self::$db_host_staging, self::$db_username_staging, self::$db_password_staging, self::$db_name_staging);
-
-            if ($db->connect_errno) {
-                printf("Connection failed: %s\n", $db->connect_error);
-                return False;
-            }
-
-            return $db;
-
-        } elseif (self::$db == 'development') {
-
-            $db = new mysqli(self::$db_host_development, self::$db_username_development, self::$db_password_development, self::$db_name_development);
-
-            if ($db->connect_errno) {
-                printf("Connection failed: %s\n", $db->connect_error);
-                return False;
-            }
-
-            return $db;
-
-
-        } else {
-
-            return false;
-
-        }
+    try {
+      $this->koneksi = new PDO("$this->driver_server:host=$this->server_database;dbname=$this->nama_database", $this->username_koneksi, $this->password_koneksi);
+      return $this->koneksi;
+    } catch (Exception $ex) {
+      return $ex;
     }
+  }
+
 }
-//usage
-//$db = new Connection('production');
